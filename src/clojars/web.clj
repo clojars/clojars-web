7f4c257cbdf31e9d2a893a77d8aab7fae54411a3
Alex Osborne <ato@meshy.org>
2009-11-18 17:04:15 +1100

    Implement group membership adding.

--- src/clojars/web.clj
+++ src/clojars/web.clj
@@ -241,13 +241,14 @@
     [:h2 "Your groups"]
     (unordered-list (map group-link (find-groups account)))))
 
-(defn show-group [account group members]
+(defn show-group [account group members & errors]
   (html-doc account (str group " group")
     [:h1 (str group " group")]
     [:h2 "Jars"]
     (unordered-list (map jar-link (jars-by-group group)))
     [:h2 "Members"]    
     (unordered-list (map user-link members))
+    (error-list errors)
     (when (some #{account} members)
       [:div {:class :add-member}
        (form-to [:post (str "/groups/" group)]
@@ -306,6 +307,22 @@
      (if-let [members (with-db (group-members group))]
        (try-account
         (show-group account group members))
+       :next)))
+  (POST #"/groups/([^/]+)"
+    (let [group ((:route-params request) 0)]
+     (if-let [members (with-db (group-members group))]
+       (try-account
+        (cond
+          (some #{(params :user)} members)
+          (show-group account group members "They're already a member!")
+          (and (some #{account} members) 
+               (find-user (params :user)))
+          (do (add-member group (params :user))
+              (show-group account group 
+                          (conj members (params :user))))
+          :else
+          (show-group account group members (str "No such user: " 
+                                                 (h (params :user))))))
        :next)))
   (GET "/users/:username"
     (if-let [user (find-user (param :username))]
