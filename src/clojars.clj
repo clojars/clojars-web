590254b16458ad7e02513fcea4fe6071ca41a78b
Phil Hagelberg <technomancy@gmail.com>
2009-11-20 21:31:49 -0800

    Create clojars/config map that reads from config.clj.

--- src/clojars.clj
+++ src/clojars.clj
@@ -1,1 +1,7 @@
-(ns clojars)
+(ns clojars
+  (:use [clojure.contrib.duck-streams :only [slurp*]]))
+
+(def config
+     (read-string (slurp* (.getResourceAsStream
+                           (.getContextClassLoader (Thread/currentThread))
+                           "clojars/config.clj"))))
---
+++ src/clojars/config.clj
@@ -0,0 +1,4 @@
+{:db {:classname "org.sqlite.JDBC"
+      :subprotocol "sqlite"
+      :subname "/home/clojars/data/db"}
+ :key-file "/home/clojars/data/auth_keys"}
