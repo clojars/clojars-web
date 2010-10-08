2ab2fc8f3bb51e8171533f135d943d212380361e
Scott Jaderholm <jaderholm@gmail.com>
2010-10-08 18:12:33 -0400

    Added username and date to search results and changed formatting

--- src/clojars/web/search.clj
+++ src/clojars/web/search.clj
@@ -3,12 +3,29 @@
         clojars.db
         compojure))
 
+(defn format-date [s]
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") s))
+
 (defn search [account query]
   (html-doc account (str (h query) " - search")
     [:h1 "Search for " (h query)]
     [:ul
-     (for [jar (search-jars query)]
-       [:li {:class "search-results"}
+     (for [jar (search-jars query)
+           ;; bc too lazy to see why blank entries are showing up
+           :when (not (nil? (:jar_name jar)))]
+       [:li.search-results
         (jar-link jar) " " (h (:version jar))
-        [:span {:class "desc"} " &mdash; "
-         (h (:description jar))]])]))
+        [:br]
+        (let [description (:description jar)]
+          (when (and (not= "" description)
+                     (not (nil? description)))
+            [:span.desc (h description)
+             [:br]]))
+        [:span.details
+         (user-link (:user jar))
+         " "
+         (if-let [created (:created jar)]
+           [:td (format-date created)])]])]))
+
+
+
