05a499662c23885c5587970f66da6740d02b4f27
Alex Osborne <ato@meshy.org>
2010-12-03 22:29:01 +1100

    Write to a writer, don't print to a writer.

--- src/clojars/db.clj
+++ src/clojars/db.clj
@@ -36,10 +36,11 @@
      (with-query-results rs ["select user, ssh_key from users"]
        (with-open [f (io/writer new-file)]
          (doseq [x rs]
-           (.println f (str "command=\"ng --nailgun-port 8700 clojars.scp " (:user x) 
+           (.write f (str "command=\"ng --nailgun-port 8700 clojars.scp " (:user x) 
                             "\"," ssh-options " " 
                             (.replaceAll (.trim (:ssh_key  x)) 
-                                               "[\n\r\0]" ""))))))
+                                         "[\n\r\0]" "")
+                            "\n")))))
      (.renameTo new-file (File. path)))))
 
 (defn db-middleware
