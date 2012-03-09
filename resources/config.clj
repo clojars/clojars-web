cbcaeda6d495878c2c0dc373d73b3c0be0cc93a5
Phil Hagelberg <technomancy@gmail.com>
2012-03-09 14:18:44 -0800

    Use absolute path in :repo config for now.

--- resources/config.clj
+++ resources/config.clj
@@ -2,7 +2,7 @@
       :subprotocol "sqlite"
       :subname "data/db"}
  :key-file "data/authorized_keys"
- :repo "data/repo"
+ :repo "/home/clojars/repo"
  :bcrypt-work-factor 12
  :mail {:hostname "smtp.gmail.com"
         :username "clojars@pupeno.com"
