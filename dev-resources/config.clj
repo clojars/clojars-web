2b75b691522c9e61c0d8b3abf06ba927e1fcd669
Nelson Morris <nmorris@nelsonmorris.net>
2012-03-16 19:04:49 -0500

    Convert to lein 2

---
+++ dev-resources/config.clj
@@ -0,0 +1,12 @@
+{:db {:classname "org.sqlite.JDBC"
+      :subprotocol "sqlite"
+      :subname "data/db"}
+ :key-file "data/authorized_keys"
+ :repo "data/repo"
+ :bcrypt-work-factor 12
+ :mail {:hostname "smtp.gmail.com"
+        :from "noreply@clojars.org"
+        :username "clojars@pupeno.com"
+        :password "fuuuuuu"
+        :port 465 ; If you change ssl to false, the port might not be effective, search for .setSSL and .setSslSmtpPort
+        :ssl true}}
--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject clojars-web "0.7.0-SNAPSHOT"
-  :aot [clojars.scp]
-  :main clojars.main
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.3"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]
@@ -15,8 +13,13 @@
                  [org.clojars.ato/nailgun "0.7.1"]
                  [org.xerial/sqlite-jdbc "3.6.17"]
                  [org.apache.commons/commons-email "1.2"]]
-  :dev-dependencies [[lein-ring "0.6.2"]
-                     [kerodon "0.0.4"]
-                     [nailgun-shim "0.0.1"]]
-  :ring {:handler clojars.web/clojars-app})
+  :profiles {:test {:resource-paths ["test-resources"]
+                    :dependencies [[kerodon "0.0.4"]
+                                   [nailgun-shim "0.0.1"]]}}
+  :plugins [[lein-ring "0.6.3"]]
+  :ring {:handler clojars.web/clojars-app}
+  :aot [clojars.scp]
+  :main clojars.main
+  :min-lein-version "2.0.0")
+
 
