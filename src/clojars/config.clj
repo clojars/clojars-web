cf19b57360ca915beddf08272b1bd37cc838e364
Alex Osborne <ato@meshy.org>
2012-04-11 13:37:37 +1000

    Do not read resources/config.clj when compiling.
    Fixes lein jar and lein uberjar.

--- src/clojars/config.clj
+++ src/clojars/config.clj
@@ -1,4 +1,4 @@
 (ns clojars.config
   (:require [clojure.java.io :as io]))
 
-(def config (read-string (slurp (io/resource "config.clj"))))
+(def config (when-not *compile-files* (read-string (slurp (io/resource "config.clj")))))
