8bac2e566fd81976c4bd53db12f15a8e71dd6bac
Nelson Morris <nmorris@nelsonmorris.net>
2012-04-10 21:23:31 -0500

    remove uneeded plexus transitive dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject clojars-web "0.7.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.3"]
-                 [org.apache.maven/maven-model "3.0.4"]
+                 [org.apache.maven/maven-model "3.0.4"
+                  :exclusions
+                  [org.codehaus.plexus/plexus-utils]]
                  [com.cemerick/pomegranate "0.0.10"]
                  [compojure "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.2"]
