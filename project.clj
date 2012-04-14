d20d848010137ef7034508f916bf02b08b71a8c5
Alex Osborne <ato@meshy.org>
2012-04-14 11:34:53 +1000

    Release 0.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.7.0-SNAPSHOT"
+(defproject clojars-web "0.7.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.3"]
                  [org.clojure/tools.cli "0.2.1"]
