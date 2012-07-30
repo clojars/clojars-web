1632cf3fdf8ea7d0b3b801f2678bce18456818e9
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-29 21:13:07 -0400

    fix paths for group names with a period, fixes #61

--- test/clojars/test/unit/maven.clj
+++ test/clojars/test/unit/maven.clj
@@ -1,8 +1,22 @@
 (ns clojars.test.unit.maven
-  (:use clojure.test clojars.maven))
+  (:use clojure.test clojars.maven)
+  (:require [clojars.config :refer [config]]
+            [clojure.java.io :as io]))
 
 (deftest pom-to-map-returns-corrects-dependencies
   (is (=
         [{:group_name "org.clojure", :jar_name "clojure", :version "1.3.0-beta1"}
          {:group_name "org.clojurer", :jar_name "clojure", :version "1.6.0"}]
      (:dependencies (pom-to-map "test-resources/test-maven/test-maven.pom")))))
+
+(deftest directory-for-handles-normal-group-name
+  (is (= (io/file (config :repo) "fake" "test" "1.0.0")
+         (directory-for {:group_name "fake"
+                         :jar_name "test"
+                         :version "1.0.0"})))
+         )
+(deftest directory-for-handles-group-names-with-dots
+  (is (= (io/file (config :repo) "com" "novemberain" "monger" "1.2.0-alpha1")
+         (directory-for {:group_name "com.novemberain"
+                         :jar_name "monger"
+                         :version "1.2.0-alpha1"}))))
