a775098486bf1ac011355bc9f7c2c5a720f96154
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-08-05 11:18:32 -0500

    Include a stack trace on the error page

--- src/clojars/web/error_page.clj
+++ src/clojars/web/error_page.clj
@@ -10,7 +10,9 @@
                  [:h1 "Oops!"]
                  [:p
                   "It seems as if an internal system error has occurred. Please give it another try. If it still doesn't work please "
-                  (link-to "https://github.com/ato/clojars-web/issues" "open an issue.")]))
+                  (link-to "https://github.com/ato/clojars-web/issues" "open an issue.")]
+                 [:p "Including the following stack trace would also be helpful."]
+                 [:pre.stacktrace (with-out-str (pst throwable))]))
       (status 500)
       (content-type "text/html")))
 
