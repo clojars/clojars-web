2572f9d24ed6b1b656b438dddae22128c072752f
Alex Osborne <ato@meshy.org>
2012-08-07 08:53:34 +1000

    Release 0.8.8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.8.8-SNAPSHOT"
+(defproject clojars-web "0.8.8"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
