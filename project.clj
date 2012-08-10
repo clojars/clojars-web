cc064edeae921eb5e40290372acfa8682bbf9119
Alex Osborne <ato@meshy.org>
2012-08-10 23:41:35 +1000

    Bump version to 0.8.11-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.8.10"
+(defproject clojars-web "0.8.11-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
