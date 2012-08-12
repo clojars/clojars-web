dd0b2620f3c89c2e97ac46b33e36e4b12df7a3ea
Alex Osborne <ato@meshy.org>
2012-08-12 17:27:36 +1000

    Layout: remove duplicate html tag and correctly close body.

--- src/clojars/web/common.clj
+++ src/clojars/web/common.clj
@@ -11,42 +11,40 @@
    "<![endif]-->"))
 
 (defn html-doc [account title & body]
-  (html5
-   [:html {:lang :en}
-    [:head
-     [:link {:type "application/opensearchdescription+xml" 
-	      :href "/opensearch.xml"
-	      :rel "search"}] 
-     [:meta {:charset "utf-8"}]
-     [:title
-      (when title
-        (str title " | "))
-      "Clojars"]
-     (map #(include-css (str "/stylesheets/" %))
-          ["reset.css" "grid.css" "screen.css"])
-     (when-ie (include-js "/js/html5.js"))]
-
-    [:body
-     [:div {:class "container_12 header"}
-      [:header
-       [:hgroup {:class :grid_4}
-        [:h1 (link-to "/" "Clojars")]
-        [:h2 "Simple Clojure jar repository"]]
-       [:nav
-        (if account
-          (unordered-list
-           [(link-to "/" "dashboard")
-            (link-to "/profile" "profile")
-            (link-to "/logout" "logout")])
-          (unordered-list
-           [(link-to "/login" "login")
-            (link-to "/register" "register")]))
-        (form-to [:get "/search"]
-          [:input {:name "q" :id "search" :class :search :value
-                   "Search jars..."
-                   :onclick (str "if (!this.cleared==1) {this.value=''; "
-                                 "this.cleared=1;}")}])]]
-      [:div {:class :clear}]]]
+  (html5   
+   [:head
+    [:link {:type "application/opensearchdescription+xml" 
+            :href "/opensearch.xml"
+            :rel "search"}] 
+    [:meta {:charset "utf-8"}]
+    [:title
+     (when title
+       (str title " | "))
+     "Clojars"]
+    (map #(include-css (str "/stylesheets/" %))
+         ["reset.css" "grid.css" "screen.css"])
+    (when-ie (include-js "/js/html5.js"))]
+   [:body
+    [:div {:class "container_12 header"}
+     [:header
+      [:hgroup {:class :grid_4}
+       [:h1 (link-to "/" "Clojars")]
+       [:h2 "Simple Clojure jar repository"]]
+      [:nav
+       (if account
+         (unordered-list
+          [(link-to "/" "dashboard")
+           (link-to "/profile" "profile")
+           (link-to "/logout" "logout")])
+         (unordered-list
+          [(link-to "/login" "login")
+           (link-to "/register" "register")]))
+       (form-to [:get "/search"]
+                [:input {:name "q" :id "search" :class :search :value
+                         "Search jars..."
+                         :onclick (str "if (!this.cleared==1) {this.value=''; "
+                                       "this.cleared=1;}")}])]]
+     [:div {:class :clear}]]
     [:div {:class "container_12 article"}
      [:article
       body]]
