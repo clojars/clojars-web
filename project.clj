1d26f0acc15bf397a44c5f996488fd9ab125082e
Phil Hagelberg <technomancy@gmail.com>
2012-11-15 06:57:39 -0800

    Call it 0.12.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.11.3-SNAPSHOT"
+(defproject clojars-web "0.12.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
