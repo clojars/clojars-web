0e11e34532dc64e64530a4944b078cb262ff266a
Phil Hagelberg <technomancy@gmail.com>
2012-11-17 13:41:49 -0800

    Release 0.12.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.12.2"
+(defproject clojars-web "0.12.3"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
