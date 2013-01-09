41f59583d017e2def2c66ae03dca4009c7464721
Phil Hagelberg <technomancy@gmail.com>
2013-01-08 20:01:43 -0800

    Switch search page over to using Lucene.

--- src/clojars/web/search.clj
+++ src/clojars/web/search.clj
@@ -1,22 +1,22 @@
 (ns clojars.web.search
   (:require [clojars.web.common :refer [html-doc jar-link user-link format-date]]
-            [clojars.db :refer [search-jars]]
+            [clojars.search :as search]
             [hiccup.core :refer [h]]
             [cheshire.core :as json]))
 
 (defn- jar->json [jar]
-  (let [m {:jar_name (:jar_name jar)
-           :group_name (:group_name jar)
+  (let [m {:jar_name (:artifact-id jar)
+           :group_name (:group-id jar)
            :version (:version jar)
            :description (:description jar)}
-        created (:created jar)]
+        created (:at jar)]
     (if created
       (assoc m :created created)
       m)))
 
 (defn json-gen [query]
   (let [results (filter #(not (nil? (:jar_name %)))
-                        (search-jars query))]
+                        (search/search query))]
     (json/generate-string {:count (count results)
                            :results (map jar->json results)})))
 
@@ -29,20 +29,18 @@
   (html-doc account (str (h query) " - search")
     [:h1 "Search for " (h query)]
     [:ul
-     (for [jar (search-jars query)
+     (for [jar (search/search query)
            ;; bc too lazy to see why blank entries are showing up
-           :when (not (nil? (:jar_name jar)))]
+           :when (not (nil? (:artifact-id jar)))]
        [:li.search-results
-        (jar-link jar) " " (h (:version jar))
+        (jar-link {:jar_name (:artifact-id jar)
+                   :group_name (:group-id jar)}) " " (h (:version jar))
         [:br]
         (when (seq (:description jar))
           [:span.desc (h (:description jar))
             [:br]])
-        [:span.details
-         (user-link (:user jar))
-         " "
-         (if-let [created (:created jar)]
-           [:td (format-date created)])]])]))
+        [:span.details (if-let [created (:created jar)]
+                         [:td (format-date created)])]])]))
 
 (defn search [account params]
   (let [q (params :q)]
