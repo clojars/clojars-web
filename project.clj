a1c9c42afaedba0b8dd3dfe5c043b5c02cbb95b3
Nelson Morris <nmorris@nelsonmorris.net>
2013-05-10 21:26:45 -0500

    Release 0.13.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.13.7"
+(defproject clojars-web "0.13.8"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
