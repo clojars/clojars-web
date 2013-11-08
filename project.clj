c3c9bfb52b67640a8488a0684c45f1b16ce33025
Alex Osborne <ato@meshy.org>
2013-11-08 11:23:21 +1100

    Release 0.14.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.14.9"
+(defproject clojars-web "0.14.10"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
