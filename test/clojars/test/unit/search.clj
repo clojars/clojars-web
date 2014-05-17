a229e3511451f0aa4ceadc99cc72f7c61c7ab1e8
wiseman <jjwiseman@gmail.com>
2014-05-16 18:59:05 -0700

    Improve search result quality.
    This should fix issues #197 and #194.
    
    We no longer give so much weight to the number of downloads.
    Previously, the number of downloads could change an item's search
    score by a factor of about 500,000x (for lein-ring, with approx. 500K
    downloads).  This meant that a search that had any terms in common
    with popular items almost always returned those items at the top, even
    if there was an item that had a perfect text match.
    
    With this change, lein-ring's downloads would only give it a
    multiplier of about 2.43x.

--- test/clojars/test/unit/search.clj
+++ test/clojars/test/unit/search.clj
@@ -6,22 +6,23 @@
 (use-fixtures :each help/default-fixture)
 
 (deftest weight-by-downloads
-  (help/make-download-count! {["asd" "alfred"] {"0.0.1" 1}
-                              ["b" "alfred"] {"0.1.0" 5}
-                              ["c" "c"] {"0.1.0" 5}})
-  (help/make-index! [{:artifact-id "alfred"
-                      :group-id "asd"}
-                     {:artifact-id "alfred"
-                      :group-id "b"}
+  (help/make-download-count! {["lein-ring" "lein-ring"] {"0.0.1" 10000}
+                              ["lein-modules" "lein-modules"] {"0.1.0" 200}
+                              ["c" "c"] {"0.1.0" 100000}})
+  (help/make-index! [{:artifact-id "lein-ring"
+                      :group-id "lein-ring"}
+                     {:artifact-id "lein-modules"
+                      :group-id "lein-modules"}
                      {:artifact-id "c"
                       :group-id "c"}])
-  (is (= (search/search "alfred")
-         [{:artifact-id "alfred"
-           :group-id "b"}
-          {:artifact-id "alfred"
-           :group-id "asd"}]))
-  (is (= (search/search "asd alfred")
-         [{:artifact-id "alfred"
-           :group-id "asd"}
-          {:artifact-id "alfred"
-           :group-id "b"}])))
+  (is (= (search/search "lein-modules")
+         [{:group-id "lein-modules", :artifact-id "lein-modules"}
+          {:group-id "lein-ring", :artifact-id "lein-ring"}]))
+  (is (= (search/search "lein-ring")
+         [{:group-id "lein-ring", :artifact-id "lein-ring"}
+          {:group-id "lein-modules", :artifact-id "lein-modules"}]))
+  (is (= (search/search "lein")
+         [{:group-id "lein-ring", :artifact-id "lein-ring"}
+          {:group-id "lein-modules", :artifact-id "lein-modules"}]))
+  (is (= (search/search "ring")
+         [{:group-id "lein-ring", :artifact-id "lein-ring"}])))
