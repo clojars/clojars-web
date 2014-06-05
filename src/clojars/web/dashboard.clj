d0df2e3f64d03c260aa7a77717cd738fd95de1a4
Devin Walters <devinw@gmail.com>
2014-06-05 10:55:58 -0500

    WIP: All the things

--- src/clojars/web/dashboard.clj
+++ src/clojars/web/dashboard.clj
@@ -3,38 +3,41 @@
             [clojars.db :refer [jars-by-username find-groupnames recent-jars]]
             [hiccup.element :refer [unordered-list link-to]]))
 
+(defn recent-jar [title description]
+  [:li.recent-jar
+   [:div.jar
+    [:h3.recent-title
+     [:a {:href "#"} title]]
+    [:p.description description]]])
+
 (defn index-page [account]
   (html-doc account nil
-    [:p "Clojars is a " [:strong "dead easy"] " community repository for"
-     " open source " (link-to "http://clojure.org/" "Clojure")
-     " libraries. "]
-    [:div {:class "useit"}
+    [:div {:class "useit-lein"}
+     [:h3 "Push with Leiningen"]
      [:div {:class "lein"}
-      [:h3 "pushing with leiningen"]
       [:pre
        (tag "$") " lein pom\n"
-       (tag "$") " scp pom.xml mylib.jar clojars@clojars.org:"]]
-
-     "It's the " [:strong "default repository"] " for "
-     (link-to "http://leiningen.org" "Leiningen")
-     ", but you can use it with other build tools like "
-     (link-to "http://maven.apache.org/" "Maven") " as well."
+       (tag "$") " scp pom.xml mylib.jar clojars@clojars.org:"]]]
+    [:div {:class "useit-maven"}
+     [:h3 "Maven Repository"]
      [:div {:class "maven"}
-      [:h3 "maven repository"]
       [:pre
        (tag "<repository>\n")
        (tag "  <id>") "clojars.org" (tag "</id>\n")
        (tag "  <url>") "http://clojars.org/repo" (tag "</url>\n")
        (tag "</repository>")]]]
-
-    [:p "To " [:strong "get started"] " pushing your own projects "
-     (link-to "/register" "create an account")
-     " and then check out the "
-     (link-to "http://wiki.github.com/ato/clojars-web/tutorial"
-              "tutorial") ". Alternatively, "
-     (link-to "/projects" "browse") " the repository."]
-    [:h2 "Recently pushed projects"]
-    (unordered-list (map jar-link (recent-jars)))))
+    [:h2 {:class "recent"} "Recently pushed projects"]
+    [:ul
+     (map #(apply recent-jar %)
+          [["foo" "bar"]
+           ["bar" "baz"]
+           ["def" "ghi"]
+           ["jkl" "mno"]
+           ["pqr" "stu"]
+           ["vwx" "yz1"]])]
+    ;; TODO: Bring me back.
+    ;; (unordered-list (map jar-link (recent-jars)))
+    ))
 
 (defn dashboard [account]
   (html-doc account "Dashboard"
