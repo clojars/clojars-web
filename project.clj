6ee44389288de1e745986fef51a6a271fbc30896
Alex Osborne <aosborne@nla.gov.au>
2014-06-12 14:25:55 +1000

    Bump version to 0.15.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.15.0"
+(defproject clojars-web "0.15.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
