a92ee830226a6c061726578bbb7f9f9f8a85d1eb
Alex Osborne <ato@meshy.org>
2014-06-16 18:56:31 +1000

    Bump version to 0.15.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.15.2"
+(defproject clojars-web "0.15.3-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
