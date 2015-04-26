52045e45f756bedd88d53c95abf31fe37007e8e1
Juho Teperi <juho.teperi@iki.fi>
2015-04-26 13:19:23 +0300

    Custom SQL query to find both latest version and latest release

--- src/clojars/routes/api.clj
+++ src/clojars/routes/api.clj
@@ -1,5 +1,6 @@
 (ns clojars.routes.api
-  (:require [compojure.core :refer [GET ANY defroutes context]]
+  (:require [clojure.set :refer [rename-keys]]
+            [compojure.core :refer [GET ANY defroutes context]]
             [compojure.route :refer [not-found]]
             [clojars.db :as db]
             [clojars.web.common :as common]
@@ -19,13 +20,42 @@
                                                  versions)))
             json/generate-string)))
 
+(defn jars-by-groupname [groupname]
+    (exec-raw [(str
+              "select j.*, j2.version as latest_release "
+              "from jars j "
+              ;; Find the latest version
+              "join "
+              "(select jar_name, max(created) as created "
+              "from jars "
+              "group by group_name, jar_name) l "
+              "on j.jar_name = l.jar_name "
+              "and j.created = l.created "
+              ;; Find the latest release
+              "join "
+              "(select jar_name, max(created) as created "
+              "from jars "
+              "where version not like '%-SNAPSHOT' "
+              "group by group_name, jar_name) r "
+              "on j.jar_name = r.jar_name "
+              ;; Join with latest release
+              "join "
+              "(select jar_name, created, version from jars) as j2 "
+              "on j2.jar_name = j.jar_name "
+              "and j2.created = r.created "
+              "where j.group_name = ? "
+              "order by j.group_name asc, j.jar_name asc")
+             [groupname]]
+              :results))
+
 (defroutes routes
   (context "/api" []
     (GET ["/groups/:group-id", :group-id #"[^/]+"] [group-id]
       (let [stats (stats/all)]
-        (-> (db/jars-by-groupname group-id)
+        (-> (jars-by-groupname group-id)
             (->> (map (fn [jar]
                         (-> jar
+                            (rename-keys {:version :latest_version})
                             (dissoc :id :created :promoted_at)
                             (assoc :downloads (stats/download-count stats group-id (:jar_name jar)))))))
             json/generate-string)))
