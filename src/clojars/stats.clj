ba879ad8074a6dfe1202c688d83f7475e8e197da
Toby Crawley <toby@tcrawley.org>
2015-05-13 16:05:40 -0400

    Memoize stats/all for 1 hour since the api uses it heavily

--- src/clojars/stats.clj
+++ src/clojars/stats.clj
@@ -1,14 +1,17 @@
 (ns clojars.stats
   (:require [clojars.config :as config]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.core.memoize :as memo]))
 
-(defn all []
+(defn all* []
   (let [path (str (config/config :stats-dir) "/all.edn")]
     (if (.exists (io/as-file path))
       (read (java.io.PushbackReader. (java.io.FileReader.
                                       (str (config/config :stats-dir)
                                            "/all.edn"))))
       {})))
+
+(def all (memo/ttl all* :ttl/threshold (* 60 60 1000))) ;; 1 hour
 
 (defn download-count [dls group-id artifact-id & [version]]
   (let [ds (dls [group-id artifact-id])]
