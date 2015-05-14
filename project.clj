a2c8e9b4a158b514a4097ea152db60ee1b222be4
Toby Crawley <toby@tcrawley.org>
2015-05-14 11:34:38 -0400

    Version 0.16.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.16.1-SNAPSHOT"
+(defproject clojars-web "0.16.1"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.apache.maven/maven-model "3.0.4"
