4c2c77642966d1017d866b5de38831a5f4787d79
Toby Crawley <toby@tcrawley.org>
2015-07-30 19:01:19 -0400

    Version 0.16.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "0.16.8-SNAPSHOT"
+(defproject clojars-web "0.16.8"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [yeller-clojure-client "1.2.1"]
