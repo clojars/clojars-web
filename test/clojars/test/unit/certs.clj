90641f2ca9786282959b157c82c43b3ff7abbe59
Toby Crawley <toby@tcrawley.org>
2015-09-04 09:32:33 -0400

    Add tests to warn when certs/keys expire.
    Start failing 60 days before the security GPG key or the site
    certificate will expire.

---
+++ test/clojars/test/unit/certs.clj
@@ -0,0 +1,16 @@
+(ns clojars.test.unit.certs
+  (:require [clojure.test :refer :all]))
+
+(def sixty-days (* 86400 1000 60))
+
+(deftest fail-when-gpg-key-is-about-to-expire
+  (let [expire-date #inst "2017-09-04T00:00:00.000-00:00"]
+    (is (< (System/currentTimeMillis)
+          (- (.getTime expire-date) sixty-days))
+      (format "Security GPG key expires on %s" expire-date))))
+
+(deftest fail-when-tls-cert-is-about-to-expire
+  (let [expire-date #inst "2016-08-12T00:00:00.000-00:00"]
+    (is (< (System/currentTimeMillis)
+          (- (.getTime expire-date) sixty-days))
+      (format "clojars.org TLS cert expires on %s" expire-date))))
