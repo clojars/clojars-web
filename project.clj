7e915e4a3afec0b17eefe89f60ece41e2cf19bc3
Toby Crawley <toby@tcrawley.org>
2016-01-20 17:50:22 -0500

    Version 30-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "29"
+(defproject clojars-web "30-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.2.1"]
