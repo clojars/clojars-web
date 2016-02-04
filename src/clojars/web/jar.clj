clojars/clojars-web

commit f89ec309e8d766203108973fbfa3179230dc604c

Fill the query input box with the current query.

src/clojars/web/jar.clj
@@ -81,7 +81,7 @@
                        (submit-button "Promote")))))))
 
 (defn show-jar [db reporter stats account jar recent-versions count]
-  (html-doc account (str (:jar_name jar) " " (:version jar))
+  (html-doc (str (:jar_name jar) " " (:version jar)) {:account account}
             (let [pom-map (jar-to-pom-map reporter jar)]
               [:div.light-article.row
                [:div#jar-title.col-sm-9.col-lg-9.col-xs-12.col-md-9
@@ -182,7 +182,7 @@
                 ]])))
 
 (defn show-versions [account jar versions]
-  (html-doc account (str "all versions of "(jar-name jar))
+  (html-doc account (str "all versions of "(jar-name jar)) {:account account}
             [:div.light-article
              [:h1 "all versions of "(jar-link jar)]
              [:div.versions

src/clojars/web.clj
@@ -57,7 +57,7 @@
          (browse db account params)))
    (GET "/security" []
         (try-account
-         (html-doc account "Security"
+         (html-doc "Security" {:account account}
                    (raw (slurp (io/resource "security.html"))))))
    session/routes
    (group/routes db)
@@ -71,8 +71,7 @@
    (ANY "*" _
         (try-account
          (not-found
-          (html-doc account
-                    "Page not found"
+          (html-doc "Page not found" {:account account}
                     [:div.small-section
                      [:h1 "Page not found"]
                      [:p "Thundering typhoons!  I think we lost it.  Sorry!"]]))))))

src/clojars/web/browse.clj
@@ -11,7 +11,7 @@
   (let [project-count (count-all-projects db)
         total-pages (-> (/ project-count per-page) Math/ceil .intValue)
         projects (browse-projects db page per-page)]
-    (html-doc account "All projects"
+    (html-doc "All projects" {:account account}
      [:div.light-article.row
       [:h1 "All projects"]
       [:div.small-section

src/clojars/web/common.clj
@@ -61,7 +61,7 @@
 (defn typekit-js []
   [:script "try{Typekit.load();}catch(e){}"])
 
-(defn html-doc [account title & body]
+(defn html-doc [title ctx & body]
   (html5
    [:head
     [:link {:type "application/opensearchdescription+xml"
@@ -98,9 +98,10 @@
                  :id "search"
                  :class "search"
                  :placeholder "Search projects..."
+                 :value (:query ctx)
                  :required true}]]]
       [:nav.main-navigation.col-md-6.col-sm-6.col-xs-12.col-lg-6
-       (if account
+       (if (:account ctx)
          (unordered-list
           [(link-to "/" "dashboard")
            (link-to "/profile" "profile")
@@ -111,7 +112,7 @@
      body
      footer]]))
 
-(defn html-doc-with-large-header [account title & body]
+(defn html-doc-with-large-header [title ctx & body]
   (html5
    [:head
     [:link {:type "application/opensearchdescription+xml"
@@ -159,7 +160,7 @@
        [:h1
         (link-to "/" "Clojars")]]
       [:nav.main-navigation.col-md-6.col-sm-6.col-xs-12.col-lg-6
-       (if account
+       (if (:account ctx)
          (unordered-list
           [(link-to "/" "dashboard")
            (link-to "/profile" "profile")
@@ -181,6 +182,7 @@
                 :name "q"
                 :id "search"
                 :placeholder "Search projects..."
+                :value (:query ctx)
                 :autofocus true
                 :required true}]
        [:input {:id "search-button"

src/clojars/web/dashboard.clj
@@ -20,7 +20,7 @@
                                                                    (:jar_name jar-map))]]]))
 
 (defn index-page [db stats account]
-  (html-doc-with-large-header account nil
+  (html-doc-with-large-header nil {:account account}
     [:article.row
      [:div.push-information.col-md-6.col-lg-6.col-sm-6.col-xs-12
       [:h3.push-header "Push with Leiningen"]
@@ -41,7 +41,7 @@
     [:ul.recent-jars-list.row (map #(recent-jar stats %) (recent-jars db))]))
 
 (defn dashboard [db account]
-  (html-doc account "Dashboard"
+  (html-doc "Dashboard" {:account account}
     [:div.light-article.col-md-12.col-lg-12.col-xs-12.col-sm-12
      [:h1 (str "Dashboard (" account ")")]
      [:div.col-md-4.col-lg-4.col-sm-4.col-xs-12

src/clojars/web/error_page.clj
@@ -4,8 +4,7 @@
             [hiccup.element :refer [link-to]]))
 
 (defn error-page-response [error-id]
-  (-> (response (html-doc nil
-                 "Oops, we encountered an error"
+  (-> (response (html-doc "Oops, we encountered an error" {}
                  [:div.small-section
                   [:h1 "Oops!"]
                   [:p

src/clojars/web/group.clj
@@ -7,7 +7,7 @@
             [clojars.web.safe-hiccup :refer [form-to]]))
 
 (defn show-group [db account groupname membernames & errors]
-  (html-doc account (str groupname " group")
+  (html-doc (str groupname " group") {:account account}
     [:div.small-section.col-md-6.col-lg-6.col-sm-6.col-xs-12
      [:h1 (str groupname " group")]
      [:h2 "Projects"]

src/clojars/web/login.clj
@@ -7,7 +7,7 @@
             [clojars.web.safe-hiccup :refer [form-to]]))
 
 (defn login-form [login_failed username]
-  (html-doc nil "Login"
+  (html-doc "Login" {}
    [:div.small-section
     [:h1 "Login"]
     [:p.hint "Don't have an account? "

commit f48121a70fd66be9acb5b3dc20e304b5a5fbcc8c

Use the correct title for the jar versions page
A typo in f89ec309e8d766203108973fbfa3179230dc604c caused the currently
logged in account name to be used as the title for the versions page,
and caused the current account to be displayed at the top of the page.

Thanks to @peeja for the report.

src/clojars/web/jar.clj
@@ -168,7 +168,7 @@
                 ]])))
 
 (defn show-versions [account jar versions]
-  (html-doc account (str "all versions of "(jar-name jar)) {:account account}
+  (html-doc (str "all versions of "(jar-name jar)) {:account account}
             [:div.light-article
              [:h1 "all versions of "(jar-link jar)]
              [:div.versions