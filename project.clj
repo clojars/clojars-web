1a1b2264017b908895af41512c2b1db6e50b2fdc
Daniel Compton <desk@danielcompton.net>
2016-02-11 12:28:01 +1300

    Upgrade Clojure to 1.8

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojars-web "31-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [yeller-clojure-client "1.4.1"]
                  [org.apache.maven/maven-model "3.0.4"
