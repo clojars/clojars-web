61a0ae4477da928c277a52bcb2118a8cb9a849a0
Toby Crawley <toby@tcrawley.org>
2016-06-21 18:45:06 -0400

    Version 41-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "40"
+(defproject clojars-web "41-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
