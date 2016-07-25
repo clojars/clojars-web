41201ea4c6f7323451e2a6981585d79387b73bca
Daniel Compton <desk@danielcompton.net>
2016-07-26 10:33:30 +1200

    Version 47-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "46"
+(defproject clojars-web "47-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
