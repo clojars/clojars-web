0acba5b2724e8e7448cd585f3638b482f0e54b46
Daniel Compton <desk@danielcompton.net>
2016-09-19 20:30:31 +1200

    Catch exceptions during startup and print them to stderr

--- src/clojars/main.clj
+++ src/clojars/main.clj
@@ -26,12 +26,17 @@
                       (:cloudfiles-container config)))))
 
 (defn -main [& args]
-  (configure args)
-  (println "clojars-web: enabling yeller client")
-  (let [yeller (yeller/client {:token (:yeller-token config)
-                               :environment (:yeller-environment config)})]
-    (Thread/setDefaultUncaughtExceptionHandler yeller)
-    (let [system (component/start (prod-system config yeller))]
-      (println "clojars-web: starting jetty on" (str "http://" (:bind config) ":" (:port config)))
-      (admin/init (get-in system [:db :spec])
-                  (:search system)))))
+  (try
+    (configure args)
+    (println "clojars-web: enabling yeller client")
+    (let [yeller (yeller/client {:token       (:yeller-token config)
+                                 :environment (:yeller-environment config)})]
+      (Thread/setDefaultUncaughtExceptionHandler yeller)
+      (let [system (component/start (prod-system config yeller))]
+        (println "clojars-web: starting jetty on" (str "http://" (:bind config) ":" (:port config)))
+        (admin/init (get-in system [:db :spec])
+                    (:search system))))
+    (catch Throwable t
+      (binding [*out* *err*]
+        (println "Error during app startup:"))
+      (.printStackTrace t))))
