a292d785637a91b4b6578febcef76d38881276b2
Alan Moore <kahunamoore@gmail.com>
2016-10-15 00:43:09 -0700

    First pass at converting from Yeller to Sentry error tracking services. Uses raven-clj to do the reporting.

--- src/clojars/errors.clj
+++ src/clojars/errors.clj
@@ -1,21 +1,28 @@
 (ns clojars.errors
-  (:require [yeller.clojure.ring :as yeller-ring]
-            [yeller.clojure.client :as yeller]
+  (:require [raven-clj.core :as raven-clj]
+            [raven-clj.interfaces :as interfaces]
             [clojars.config :refer [config]]
             [clj-stacktrace.repl :refer [pst]]
             [clojars.web.error-page :as error-page]
             [clojars.web.error-api :as error-api])
-  (:import java.util.UUID
-           com.yellerapp.client.YellerHTTPClient))
+  (:import java.util.UUID))
 
 
 (defprotocol ErrorReporter
   (-report-error [reporter e extra id]))
 
-(extend-protocol ErrorReporter
-  YellerHTTPClient
-  (-report-error [client e extra id]
-    (yeller/report client e (assoc-in extra [:custom-data :error-id] id))))
+(defrecord RavenErrorReporter [raven-config]
+  ErrorReporter
+  (-report-error [_ e _ id]
+    (raven-clj/capture (:dsn raven-config)
+                       (-> {:message "RavenErrorReporter capture" :error-id id}
+                           (interfaces/stacktrace e))))
+
+  java.lang.Thread$UncaughtExceptionHandler
+  (uncaughtException [_ thread throwable]
+    (raven-clj/capture (:dsn raven-config)
+                       (-> {:message "UncaughtExceptionHandler capture"}
+                           (interfaces/stacktrace throwable)))))
 
 (defrecord StdOutReporter []
   ErrorReporter
@@ -43,8 +50,20 @@
        (-report-error reporter e extra id))
      id)))
 
+(defn alter-fn [request]
+  ;(dissoc request :data)) ; TODO:
+  request)
+
 (defn report-ring-error [reporter e request]
-  (report-error reporter e (yeller-ring/format-extra nil request)))
+  (raven-clj/capture (:dsn reporter)
+                     (-> {:message "Ring reported an error"}
+                         (interfaces/stacktrace e ["clojars"])
+                         (interfaces/http request alter-fn))))
+
+; Note: I did not use raven-ring because this exisitng logic wraps ring exceptions
+; and also handles user error reporting, etc. I left the implementation as close
+; to the original yeller code as possible - just to keep the changes to a minimum.
+; See also: [raven-clj.ring :as raven-ring]
 
 (defn wrap-exceptions [app reporter]
   (fn [req]
--- src/clojars/main.clj
+++ src/clojars/main.clj
@@ -3,23 +3,23 @@
              [admin :as admin]
              [cloudfiles :as cf]
              [config :refer [config configure]]
-             [errors :refer [->StdOutReporter multiple-reporters]]
+             [errors :refer [->StdOutReporter ->RavenErrorReporter multiple-reporters]]
              [system :as system]]
             [com.stuartsierra.component :as component]
             [meta-merge.core :refer [meta-merge]]
-            [yeller.clojure.client :as yeller])
+            [raven-clj.core :as raven-clj])
   (:gen-class))
 
 (def prod-env
   {:app {:middleware []}})
 
-(defn prod-system [config yeller]
+(defn prod-system [config raven-reporter]
   (-> (meta-merge config prod-env)
       system/new-system
       (assoc
         :error-reporter (multiple-reporters
                           (->StdOutReporter)
-                          yeller)
+                          raven-reporter)
         :cloudfiles     (cf/connect
                           (:cloudfiles-user config)
                           (:cloudfiles-token config)
@@ -28,16 +28,15 @@
 (defn -main [& args]
   (try
     (configure args)
-    (println "clojars-web: enabling yeller client")
-    (let [yeller (yeller/client {:token       (:yeller-token config)
-                                 :environment (:yeller-environment config)})]
-      (Thread/setDefaultUncaughtExceptionHandler yeller)
-      (let [system (component/start (prod-system config yeller))]
-        (println "clojars-web: starting jetty on" (str "http://" (:bind config) ":" (:port config)))
-        (admin/init (get-in system [:db :spec])
-                    (:queue system)
-                    (:search system)
-                    (:storage system))))
+    (println "clojars-web: enabling raven-clj client dsn:project-id:" (:project-id (raven-clj/parse-dsn (:raven-dsn config))))
+    (let [raven-reporter (assoc (->RavenErrorReporter {}) :dsn (:raven-dsn config))]
+     (Thread/setDefaultUncaughtExceptionHandler raven-reporter)
+     (let [system (component/start (prod-system config raven-reporter))]
+       (println "clojars-web: starting jetty on" (str "http://" (:bind config) ":" (:port config)))
+       (admin/init (get-in system [:db :spec])
+                   (:queue system)
+                   (:search system)
+                   (:storage system))))
     (catch Throwable t
       (binding [*out* *err*]
         (println "Error during app startup:"))
