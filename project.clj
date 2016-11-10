58d5f572b857ddcce7f7591ce3677b879c2297d0
Toby Crawley <toby@tcrawley.org>
2016-11-10 12:30:20 -0500

    Version 53-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "52"
+(defproject clojars-web "53-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
