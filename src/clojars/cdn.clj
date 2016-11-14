dc3223ecd6983b04bf45a4a088aa4c84214af52c
Toby Crawley <toby@tcrawley.org>
2016-11-14 10:04:09 -0500

    Typo

--- src/clojars/cdn.clj
+++ src/clojars/cdn.clj
@@ -4,7 +4,7 @@
 
 (defn purge [key cdn-url path]
   (let [res (http/request {:method "PURGE"
-                           :url (format "%/%s" cdn-url path)
+                           :url (format "%s/%s" cdn-url path)
                            :headers {"Fastly-Key" key}})
         body (json/parse-string (:body res) true)]
     (assoc body :http-status (:status res))))
