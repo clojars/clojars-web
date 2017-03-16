b786548beb3b58ac36ee2583db9e92648a5cbf29
Daniel Compton <desk@danielcompton.net>
2017-03-17 08:19:14 +1300

    Version 60-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "59"
+(defproject clojars-web "60-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
