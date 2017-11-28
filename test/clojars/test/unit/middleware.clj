54e30590e36a47299ad71fe70452df1dc4f97a4c
Toby Crawley <toby@tcrawley.org>
2017-11-28 10:11:17 -0500

    Fix these tests to actually test

--- test/clojars/test/unit/middleware.clj
+++ test/clojars/test/unit/middleware.clj
@@ -8,10 +8,10 @@
   (is (= "/" (trailing-slash {:uri "/"}))))
 
 (deftest trailing-slash-doesnt-modify-sub-routes
-  (is (= "/artifact/project") (trailing-slash {:uri "/artifact/project"})))
+  (is (= "/artifact/project" (trailing-slash {:uri "/artifact/project"}))))
 
 (deftest trailing-slash-removes-trailing-slash
-  (is (= "/artifact/project") (trailing-slash {:uri "/artifact/project/"})))
+  (is (= "/artifact/project" (trailing-slash {:uri "/artifact/project/"}))))
 
 (deftest trailing-slash-doesnt-remove-redundant-trailing-slash
-  (is (= "/artifact/project/") (trailing-slash {:uri "/artifact/project//"})))
+  (is (= "/artifact/project/" (trailing-slash {:uri "/artifact/project//"}))))
