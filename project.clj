cb93589146042938f71df741c08864230fb9ccd6
Toby Crawley <toby@tcrawley.org>
2017-12-29 08:18:38 -0500

    Version 69-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "68"
+(defproject clojars-web "69-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
