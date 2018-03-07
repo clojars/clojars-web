51e8b0cbc9aee01a341f01e8132da1ac4f7a98be
Toby Crawley <toby@tcrawley.org>
2018-03-07 18:44:04 -0500

    Version 73-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "72"
+(defproject clojars-web "73-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]
