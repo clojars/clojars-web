8e52280730561a266c4a3830a49a218dd24e816d
Toby Crawley <toby@tcrawley.org>
2019-12-26 12:40:39 -0500

    Update to new gpg certificate

--- test/clojars/test/unit/certs.clj
+++ test/clojars/test/unit/certs.clj
@@ -4,7 +4,7 @@
 (def sixty-days (* 86400 1000 60))
 
 (deftest fail-when-gpg-key-is-about-to-expire
-  (let [expire-date #inst "2019-11-28T00:00:00.000-00:00"]
+  (let [expire-date #inst "2021-12-25T00:00:00.000-00:00"]
     (is (< (System/currentTimeMillis)
           (- (.getTime expire-date) sixty-days))
       (format "Security GPG key expires on %s" expire-date))))
