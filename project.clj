05889bd4f14e04b90708dc1229d820c11f0c31a3
Toby Crawley <toby@tcrawley.org>
2019-12-31 08:31:25 -0500

    Version 79-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "78"
+(defproject clojars-web "79-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.memoize "0.5.8"]
