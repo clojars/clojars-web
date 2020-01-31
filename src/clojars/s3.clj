960dbe8b1d274bd9690788682cccfab7a807b3ea
Toby Crawley <toby@tcrawley.org>
2020-01-31 07:14:44 -0500

    Add lazy list-objects to s3 client

--- src/clojars/s3.clj
+++ src/clojars/s3.clj
@@ -8,7 +8,29 @@
 
 (defprotocol S3
   (-get-object-stream [client bucket-name key])
+  (-list-objects [client bucket-name prefix])
   (-put-object [client bucket-name key stream]))
+
+(defn- list-objects-chunk
+  [client bucket-name prefix marker]
+  (let [request (cond-> {:Bucket bucket-name}
+                  prefix (assoc :Prefix prefix)
+                  marker (assoc :Marker marker))]
+    (aws/invoke client
+                {:op :ListObjects
+                 :request request})))
+
+(defn- list-objects-seq
+  "Generates a lazy seq of objects, chunked by the API's paging."
+  [client bucket-name prefix marker]
+  (let [{:keys [Contents IsTruncated]}
+        (list-objects-chunk client bucket-name prefix marker)]
+    (if IsTruncated
+      (lazy-seq
+        (concat Contents
+                (list-objects-seq client bucket-name prefix
+                                  (-> Contents last :Key))))
+      Contents)))
 
 (defrecord S3Client [s3]
   S3
@@ -18,6 +40,8 @@
                      :request {:Bucket bucket-name
                                :Key key}})
         :Body))
+  (-list-objects [_ bucket-name prefix]
+    (map :Key (list-objects-seq s3 bucket-name prefix nil)))
   (-put-object [_ bucket-name key stream]
     (aws/invoke s3 {:op :PutObject
                     :request {:Bucket bucket-name
@@ -41,16 +65,24 @@
   (-get-object-stream [_ bucket-name key]
     (when-let [data (get-in @state [bucket-name key])]
       (ByteArrayInputStream. data)))
+  (-list-objects [_ bucket-name _prefix]
+    (keys (get @state bucket-name)))
   (-put-object [_ bucket-name key stream]
     (swap! state assoc-in [bucket-name key] (IOUtils/toByteArray stream))))
 
 (defn mock-s3-client []
   (->MockS3Client (atom {})))
 
-(defn put-object
-  [s3 bucket-name key stream]
-  (-put-object s3 bucket-name key stream))
-
 (defn get-object-stream
   [s3 bucket-name key]
   (-get-object-stream s3 bucket-name key))
+
+(defn list-objects
+  ([s3 bucket-name]
+   (list-objects s3 bucket-name nil))
+  ([s3 bucket-name prefix]
+   (-list-objects s3 bucket-name prefix)))
+
+(defn put-object
+  [s3 bucket-name key stream]
+  (-put-object s3 bucket-name key stream))
