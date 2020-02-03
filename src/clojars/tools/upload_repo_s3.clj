f741e5e98497f6da3d43087195c6dc7d07d4972d
Toby Crawley <toby@tcrawley.org>
2020-02-02 20:34:27 -0500

    Better output from upload-repo tool
    - Print status every 1000 files processed
    - Don't count local files, that realizes the full lazy seq, which can
      be quite a bit of memory

--- src/clojars/tools/upload_repo_s3.clj
+++ src/clojars/tools/upload_repo_s3.clj
@@ -5,6 +5,15 @@
    [clojure.java.io :as io])
   (:gen-class))
 
+(defn print-status
+  [{:keys [changed new skipped]}]
+  (printf "Uploaded: %s (new: %s, changed: %s); Skipped %s\n"
+          (+ changed new)
+          new
+          changed
+          skipped)
+  (flush))
+
 (defn maybe-upload-file
   [s3-client existing repo stats f]
   (let [path (fu/subpath
@@ -12,6 +21,8 @@
                (.getAbsolutePath f))]
     ;; ETag from s3 is an md5 sum, but only for non-multipart
     ;; uploads. Luckily, we don't upload artifacts as multipart
+    (when (= 0 (rem (apply + (vals stats)) 1000))
+      (print-status stats))
     (if (= (existing path) (fu/checksum f :md5))
       (update stats :skipped inc)
       (let [reason (if (existing path) :changed :new)]
@@ -21,9 +32,10 @@
 
 (defn get-existing [s3-client subpath]
   (printf "Retrieving current artifact list [subpath: %s] (this may take a while)\n" subpath)
+  (flush)
   (into {}
-    (map (juxt :Key :ETag))
-    (s3/list-objects s3-client subpath)))
+        (map (juxt :Key :ETag))
+        (s3/list-objects s3-client subpath)))
 
 (defn local-files [repo subpath]
   (let [local-dir (if subpath (io/file repo subpath) (io/file repo))]
@@ -32,19 +44,15 @@
 (defn upload-repo [s3-client repo subpath]
   (let [existing (get-existing s3-client subpath)
         local-files (local-files repo subpath)]
-    (printf "Local files: %s; Remote files: %s\n" (count local-files) (count existing))
-    (let [{:keys [skipped changed new]}
-          (reduce
-            (partial maybe-upload-file s3-client existing repo)
-            {:skipped 0
-             :changed 0
-             :new 0}
-            local-files)]
-      (printf "Uploaded: %s (new: %s, changed: %s); Skipped %s\n"
-              (+ changed new)
-              new
-              changed
-              skipped))))
+    (println "Starting upload")
+    (flush)
+    (let [stats (reduce
+                  (partial maybe-upload-file s3-client existing repo)
+                  {:skipped 0
+                   :changed 0
+                   :new 0}
+                  local-files)]
+      (print-status stats))))
 
 (defn -main [& args]
   (if (< (count args) 5)
