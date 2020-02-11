3666c86f2e5539bb26d67109ac2c8b6dbc805544
Toby Crawley <toby@tcrawley.org>
2020-02-11 07:34:36 -0500

    Don't look for access logs in Cloudfiles
    The access logs haven't been written to cloudfiles in a while - they
    are in S3 instead.

--- src/clojars/tools/combine_cdn_logs.clj
+++ src/clojars/tools/combine_cdn_logs.clj
@@ -1,41 +1,26 @@
 (ns clojars.tools.combine-cdn-logs
-  (:require [clojars.cloudfiles :as cf]
-            [clojars.s3 :as s3]
+  (:require [clojars.s3 :as s3]
             [clojure.java.io :as io])
   (:import java.io.FileOutputStream)
   (:gen-class))
 
-;; downloads and combines cdn log files from cloudfiles and s3 for the
-;; given date, then uploads the combined file to s3 container
-
-(defn connect [username key container-name]
-  (cf/connect username key container-name))
+;; downloads and combines cdn log files from s3 for the
+;; given date, then uploads the combined file to s3 bucket
 
 (def domap (comp dorun map))
 
 (defn -main [& args]
-  (if (not= 9 (count args))
-    (println "Usage: raw-log-container cf-user cf-key s3-log-bucket aws-region aws-key aws-secret date output-file")
-    (let [[raw-container-name cf-user cf-key
-           s3-bucket aws-region aws-key aws-secret
+  (if (not= 6 (count args))
+    (println "Usage: s3-log-bucket aws-region aws-key aws-secret date output-file")
+    (let [[s3-bucket aws-region aws-key aws-secret
            date output-file] args
           date               (apply format "%s-%s-%s"
                                     (rest (re-find #"(\d{4})(\d{2})(\d{2})" date)))
           dest-file          (io/file output-file)
-          name-regex         (re-pattern (str "^" date))
-          down-conn          (connect cf-user cf-key raw-container-name)
           s3                 (s3/s3-client aws-key aws-secret aws-region s3-bucket)
-          cf-log-files       (eduction
-                               (map :name)
-                               (filter #(re-find name-regex %))
-                               (cf/metadata-seq down-conn))
           s3-log-files       (s3/list-object-keys s3 date)]
       (with-open [fos (FileOutputStream. dest-file)]
-        ;; download and combine cloudfiles logs
-        (domap #(with-open [in (cf/artifact-stream down-conn %)]
-                  (io/copy in fos))
-               cf-log-files)
-        ;; then s3 logs
+        ;; download and combine s3 logs
         (domap #(with-open [in (s3/get-object-stream s3 %)]
                   (io/copy in fos))
                s3-log-files))
