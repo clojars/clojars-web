7bab811834bf29fe1fdcccd0d3ad936acae61cb4
Toby Crawley <toby@tcrawley.org>
2020-02-13 18:35:18 -0500

    Version 95-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "94"
+(defproject clojars-web "95-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.memoize "0.8.2"]
