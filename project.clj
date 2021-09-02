c487e0c53d70afa1bc860e8ae0f44e0db131f00c
Toby Crawley <toby@tcrawley.org>
2021-09-02 10:22:59 -0400

    Version 145-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "144"
+(defproject clojars-web "145-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.memoize "0.8.2"]
