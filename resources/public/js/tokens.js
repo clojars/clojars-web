0442818aaa186868ea8c4fde5100470988a73646
Toby Crawley <toby@tcrawley.org>
2022-04-04 08:10:54 -0400

    Auto-set token name when using a single-use token
    This will set the name to a timestamp-based one, but only if one hasn't
    already been entered.

--- resources/public/js/tokens.js
+++ resources/public/js/tokens.js
@@ -10,6 +10,19 @@ $(toggleSel).change(() => {
 
 const hideTokenRows = (sel) => $(sel).hide();
 
+const enableAutoSetTokenName = () => {
+  const single_use = $('#single_use');
+  single_use.change(() => {
+    const name_input = $('#name');
+    if (single_use.is(':checked') &&
+        !name_input.val()) {
+      // drop timezone name
+      const now = new Date().toString().substring(0, 33);
+      name_input.val('<single-use ' + now + '>');
+    }
+  });
+};
+
 $(() => {
   hideTokenRows('.token-disabled');
   hideTokenRows('.token-used');
@@ -17,4 +30,5 @@ $(() => {
   configureToggleTokenRows('#show-disabled', '.token-disabled');
   configureToggleTokenRows('#show-used', '.token-used');
   configureToggleTokenRows('#show-expired', '.token-expired');
+  enableAutoSetTokenName();
 });
