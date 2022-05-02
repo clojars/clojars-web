e3d48ee0b3ed379e1603ec81f168e935316fc305
Toby Crawley <toby@tcrawley.org>
2022-05-02 07:58:56 -0400

    Add request details to user emails

--- src/clojars/notifications/common.clj
+++ src/clojars/notifications/common.clj
@@ -1,4 +1,19 @@
-(ns clojars.notifications.common)
+(ns clojars.notifications.common
+  (:import
+   (java.text SimpleDateFormat)))
 
 (def did-not-take-action
   "If you *didn't* take this action, please reply to this email to let the Clojars admins know that your account has potentially been compromised!")
+
+(defn- iso-8601 []
+  (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
+
+(defn details-table
+  [{:as _details :keys [remote-addr timestamp user-agent]}]
+  ;; We don't always have the details in every context. One example is
+  ;; mfa-disablement from using a recovery code - that happens in the bowels of
+  ;; friend, so it is difficult to capture the rquest context there.
+  (if remote-addr
+    (format "Client IP: %s\nUser agent: %s\nTimestamp: %s"
+            remote-addr user-agent (.format (iso-8601) timestamp))
+    ""))
