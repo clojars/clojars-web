cc1005d1fe96dcbf40a946730373531fa58c4738
Toby Crawley <toby@tcrawley.org>
2022-05-03 17:20:35 -0400

    Version 167-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "166"
+(defproject clojars-web "167-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.memoize "1.0.253"]
