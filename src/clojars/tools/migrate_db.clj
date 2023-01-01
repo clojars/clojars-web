25e2020d33153ec67315058ab9e5ea981c390f6e
Toby Crawley <toby@tcrawley.org>
2023-01-01 17:02:40 -0500

    Allow specifying profile to migrate-db tool
    Now that we have a single configuration, this would try to load
    development db configuration in production.

--- src/clojars/tools/migrate_db.clj
+++ src/clojars/tools/migrate_db.clj
@@ -1,10 +1,13 @@
 (ns clojars.tools.migrate-db
   (:gen-class)
   (:require
-   [clojars.config :refer [config]]
+   [clojars.config :as config]
    [clojars.db.migrate :refer [migrate]]))
 
-(defn -main [& _]
-  (let [db (:db (config))]
-    (println "=> Migrating db")
-    (migrate db)))
+(defn -main [& [env]]
+  (binding [config/*profile* (if env
+                               (keyword env)
+                               :development)]
+    (let [db (:db (config/config))]
+      (printf "=> Migrating %s db\n" config/*profile*)
+      (migrate db))))
