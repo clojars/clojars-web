ae9801234fe6554fef0837fc3a32304eece4acc0
Toby Crawley <toby@tcrawley.org>
2023-04-30 10:03:20 -0400

    Version 200-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojars-web "199"
+(defproject clojars-web "200-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.memoize "1.0.253"]
