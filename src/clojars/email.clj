0d33a469744f71aa965eac40c6a9cdebd44edefa
Toby Crawley <toby@tcrawley.org>
2023-08-12 09:50:39 -0400

    Add denylist for sending emails
    This allows us to ignore a testing account with an invalid email
    address.

--- src/clojars/email.clj
+++ src/clojars/email.clj
@@ -1,6 +1,8 @@
 (ns clojars.email
   (:require
-   [clojars.log :as log])
+   [clojars.log :as log]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io])
   (:import
    (java.util.concurrent
     CountDownLatch
@@ -8,27 +10,32 @@
    (org.apache.commons.mail
     SimpleEmail)))
 
+(def ^:private email-denylist
+  (edn/read-string (slurp (io/resource "email-denylist.edn"))))
+
 (defn simple-mailer [{:keys [hostname username password port tls? from]}]
   (fn [to subject message]
     (log/with-context {:tag :email
                        :email-to to
                        :email-subject subject}
       (try
-        (let [mail (doto (SimpleEmail.)
-                     (.setHostName (or hostname "localhost"))
-                     (.setSmtpPort (or port 25))
-                     (.setStartTLSEnabled (boolean tls?))
-                     (.setStartTLSRequired (boolean tls?))
-                     (.setFrom (or from "contact@clojars.org") "Clojars")
-                     (.addTo to)
-                     (.setSubject subject)
-                     (.setMsg message))]
-          (when tls?
-            (.setSslSmtpPort mail (str (or port 25))))
-          (when (and username password)
-            (.setAuthentication mail username password))
-          (.send mail)
-          (log/info {:status :success}))
+        (if (contains? email-denylist to)
+          (log/info {:status :denylist})
+          (let [mail (doto (SimpleEmail.)
+                       (.setHostName (or hostname "localhost"))
+                       (.setSmtpPort (or port 25))
+                       (.setStartTLSEnabled (boolean tls?))
+                       (.setStartTLSRequired (boolean tls?))
+                       (.setFrom (or from "contact@clojars.org") "Clojars")
+                       (.addTo to)
+                       (.setSubject subject)
+                       (.setMsg message))]
+            (when tls?
+              (.setSslSmtpPort mail (str (or port 25))))
+            (when (and username password)
+              (.setAuthentication mail username password))
+            (.send mail)
+            (log/info {:status :success})))
         (catch Exception e
           (log/error {:status :failed
                       :error e})
