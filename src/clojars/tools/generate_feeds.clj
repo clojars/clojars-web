158cedadd20b3f60eb46a6217729c5356e146121
Osei Poku <osei.poku@gmail.com>
2024-09-26 01:16:27 -0400

    Generating sitemaps
    - adds clojure.data.xml as a dependency
    - always generate a sitemap index file because it assumes that clojars
      has more than 50,000

--- src/clojars/tools/generate_feeds.clj
+++ src/clojars/tools/generate_feeds.clj
@@ -8,7 +8,9 @@
    [clojars.s3 :as s3]
    [clojars.util :as util]
    [clojure.java.io :as io]
-   [clojure.set :as set])
+   [clojure.data.xml :as xml]
+   [clojure.set :as set]
+   [clojars.routes.artifact :as artifact])
   (:import
    (java.io
     FileOutputStream
@@ -29,11 +31,14 @@
                                         :scm-tag (:tag scm)))
                      distinct-jars)}))
 
+(defn- all-grouped-jars [db]
+  (->> (db/all-jars db)
+       (maven/sort-by-version)
+       (reverse) ;; We want the most recent version first
+       (group-by (juxt :group_name :jar_name))))
+
 (defn full-feed [db]
-  (let [grouped-jars (->> (db/all-jars db)
-                          (maven/sort-by-version)
-                          (reverse) ;; We want the most recent version first
-                          (group-by (juxt :group_name :jar_name)))]
+  (let [grouped-jars (all-grouped-jars db)]
     (->> (for [[[group-id artifact-id] jars] grouped-jars]
            (try
              (let [base-jar (first jars)]
@@ -84,12 +89,93 @@
   [(fu/create-checksum-file f :md5)
    (fu/create-checksum-file f :sha1)])
 
+(def sitemap-ns "http://www.sitemaps.org/schemas/sitemap/0.9")
+(xml/alias-uri 'sitemap sitemap-ns)
+
+(defn links-list [base-url db]
+  (let [ ;; get current domain
+
+        base-links [ ;; index, projects, security, dmca
+                    ""
+                    "/projects"
+                    "/security"
+                    "/dmca"]
+        group-links (into []
+                          (comp
+                           (map :group_name)
+                           (map #(str "/groups/" %)))
+                          (db/all-groups db))
+
+        artifact-links (into []
+                             (mapcat
+                              (fn [[[group-id artifact-id] jars]]
+                                (let [artifact-links
+                                      (concat
+                                       [(str "/" artifact-id)
+                                        (str "/" artifact-id "/dependents")
+                                        (str "/" artifact-id "/versions")]
+                                       (into []
+                                             (map
+                                              #(str "/" artifact-id "/versions/" (:version %)))
+                                             jars))]
+                                  (if (= group-id artifact-id)
+                                     artifact-links
+                                     (map #(str "/" group-id %) artifact-links)))))
+                             (all-grouped-jars db))
+
+        user-links (into []
+                         (map :user)
+                         (db/all-users db))
+
+        all-links (concat base-links
+                          group-links
+                          artifact-links
+                          user-links)]
+    (map #(str base-url %) all-links)))
+
+(defn write-sitemap
+  "returns sitemap filename"
+  [dest index links]
+  (let [sitemap-file (str dest "/sitemap-" index ".xml.gz")
+        sitemap [::sitemap/urlset {:xmlns sitemap-ns}
+                 (for [link links]
+                   [::sitemap/url
+                    [::sitemap/loc link]])]]
+    (write-to-file [(xml/sexp-as-element sitemap)]
+                   sitemap-file
+                   :gzip
+                   #(xml/emit % *out*))))
+
+(defn write-sitemap-index
+  "returns sitemap index filename"
+  [base-url dest sitemap-files]
+  (let [sitemap-index-file (str dest "/sitemap-index.xml.gz")
+        sitemap-index [::sitemap/sitemapindex {:xmlns sitemap-ns}
+                       (for [sitemap-file sitemap-files]
+                         [::sitemap/sitemap
+                          [::sitemap/loc (str base-url "/" sitemap-file)]])]]
+    (write-to-file [(xml/sexp-as-element sitemap-index)]
+                   sitemap-index-file
+                   :gzip
+                   #(xml/emit % *out*))))
+
+(defn generate-sitemaps
+  "base-url - without the trailing slash"
+  [base-url dest db]
+  (let [sitemap-files (->> (links-list base-url db)
+                           (partition-all 50000)
+                           (map-indexed #(write-sitemap dest %1 %2))
+                           ((juxt #(write-sitemap-index base-url dest %) identity))
+                           (apply list*))
+        checksum-files (mapcat write-sums sitemap-files)]
+    (concat sitemap-files checksum-files)))
+
 (defn put-files [s3-bucket & files]
   (run! #(let [f (io/file %)]
            (s3/put-file s3-bucket (.getName f) f {:ACL "public-read"}))
         files))
 
-(defn generate-feeds [dest db s3-bucket]
+(defn generate-feeds [dest base-url db s3-bucket]
   (let [feed-file (str dest "/feed.clj.gz")]
     (apply put-files
            s3-bucket
@@ -116,10 +202,15 @@
            (write-to-file jars gz-file :gzip)
            (concat
             (write-sums jar-file)
-            (write-sums gz-file)))))
+            (write-sums gz-file))))
+
+  (apply put-files
+         s3-bucket
+         (generate-sitemaps base-url dest db)))
 
 (defn -main [feed-dir env]
-  (let [{:keys [db s3]} (config (keyword env))]
+  (let [{:keys [db s3 base-url]} (config (keyword env))]
     (generate-feeds feed-dir
+                    base-url
                     db
                     (s3/s3-client (:repo-bucket s3)))))
