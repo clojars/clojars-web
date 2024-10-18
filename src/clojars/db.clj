135badf17cd71108973927e9dec332fa9b258f6d
Toby Crawley <toby@tcrawley.org>
2024-10-18 18:31:33 -0400

    Revert "Generate sitemaps with other slow changing data"

--- src/clojars/db.clj
+++ src/clojars/db.clj
@@ -600,12 +600,6 @@
                 :from     :jars
                 :order-by :id}))))
 
-
-(defn all-groups [db]
-  (q db {:select-distinct [:group_name]
-         :from :jars
-         :order-by [[:group_name :asc]]}))
-
 (defn find-dependencies
   [db groupname jarname version]
   (q db
@@ -634,13 +628,6 @@
       :order-by [[:group_name :asc] [:jar_name :asc]]
       :limit limit-num
       :offset offset-num}))
-
-(defn all-users
-  [db]
-  (q db
-     {:select [:user]
-      :from :users
-      :order-by [[:user :asc]]}))
 
 (defn count-all-projects
   [db]
--- src/clojars/web/common.clj
+++ src/clojars/web/common.clj
@@ -41,7 +41,6 @@
      (link-to "/dmca" "DMCA")
      (link-to "https://github.com/clojars/clojars-web/wiki/" "help")
      (link-to "https://github.com/clojars/clojars-web/wiki/Data" "API")
-     (link-to "/repo/sitemap.xml" "sitemap")
      [:div.sponsors
       [:div.sponsors-title
        "Hosting costs sponsored by:"]
