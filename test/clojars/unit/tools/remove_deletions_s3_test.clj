c074fe9106ac292397ca7a05feef722fe6bb1c20
Toby Crawley <toby@tcrawley.org>
2026-01-17 09:10:17 -0500

    Extract temp dir creation to helper
    This allows us to use it in other tests. It also sets the temp dirs to
    be deleted on JVM exit.

--- test/clojars/unit/tools/remove_deletions_s3_test.clj
+++ test/clojars/unit/tools/remove_deletions_s3_test.clj
@@ -1,14 +1,10 @@
 (ns clojars.unit.tools.remove-deletions-s3-test
   (:require
    [clojars.s3 :as s3]
+   [clojars.test-helper :as help]
    [clojars.tools.remove-deletions-s3 :as remove-deletions-s3]
    [clojure.java.io :as io]
-   [clojure.test :refer [deftest is]])
-  (:import
-   (java.nio.file
-    Files)
-   (java.nio.file.attribute
-    FileAttribute)))
+   [clojure.test :refer [deftest is]]))
 
 (deftest improper-args-should-print
   (let [str (with-out-str (remove-deletions-s3/-main))]
@@ -17,7 +13,7 @@
 (deftest removal-of-missing-local
   (let [s3-client (s3/mock-s3-client)
         file (io/file (io/resource "fake.jar"))
-        repo (.toFile (Files/createTempDirectory "repo" (make-array FileAttribute 0)))
+        repo (help/create-tmp-dir "repo")
         local-file (io/file repo "foo/bar/baz.jar")]
     (s3/put-file s3-client "foo/bar/baz.jar" file)
     (s3/put-file s3-client "boo/far/baz.jar" file)
@@ -32,7 +28,7 @@
 (deftest removal-with-subpath
   (let [s3-client (s3/mock-s3-client)
         file (io/file (io/resource "fake.jar"))
-        repo (.toFile (Files/createTempDirectory "repo" (make-array FileAttribute 0)))
+        repo (help/create-tmp-dir "repo")
         local-file (io/file repo "foo/bar/baz.jar")]
     (s3/put-file s3-client "foo/bar/baz.jar" file)
     (s3/put-file s3-client "foo/far/baz.jar" file)
